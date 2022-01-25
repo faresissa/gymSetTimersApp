bee8532a59c3f03580ecdb6df9d7df2e2d90c4b0
Fares Issa <faresissa@hotmail.com>
2022-01-25 11:51:13 -0500

    Initial Commit

---
+++ clockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clockApp
+//
+//  Created by Fares Issa on 2022-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clockApp/clockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockAppApp.swift
+//  clockApp
+//
+//  Created by Fares Issa on 2022-01-25.
+//
+
+import SwiftUI
+
+@main
+struct clockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
