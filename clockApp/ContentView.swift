ae30a01de3bc0ece96bf25d78aa52bdd553df4a4
Fares Issa <faresissa@hotmail.com>
2022-02-06 15:09:23 -0500

    Added multiple sequential timers

--- clockApp/ContentView.swift
+++ clockApp/ContentView.swift
@@ -7,149 +7,85 @@
 import Combine
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var firstName = ""
-//    @State private var lastName = ""
-//    @State private var shouldSendNewsletter = false
-//    @State private var numberOfLikes = 1
-//
-//    var body: some View {
-//        NavigationView{
-//            Form {
-//                Section(header: Text("Personal Information")) {
-//                    TextField("First Name", text: $firstName)
-//                    TextField("Last Name", text: $lastName)
-//                }
-//                Section(header: Text("Actions")) {
-//                    Toggle("Send Newsletter", isOn: $shouldSendNewsletter)
-//                        .toggleStyle(SwitchToggleStyle(tint: .red))
-//                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
-//                    Text("Counter: \(numberOfLikes)")
-//                }
-//            }
-//            .navigationTitle("Account")
-//            .toolbar {
-//                ToolbarItemGroup(placement: .navigationBarTrailing) {
-//                    Button {
-//                        hideKeyboard()
-//                    } label: {
-//                        Image(systemName: "keyboard.chevron.compact.down")
-//                    }
-//                    Button("Save", action: saveUser)
-//                }
-//            }
-//            .accentColor(.red)
-//        }
-//    }
-//    func saveUser() {
-//       print("User Saved")
-//    }
-//}
-/*
-struct ContentView: View {
-    //Input Declarations
-    @State private var workoutTimeInput: Int = 0
-    @State private var superSetTimeInput: Int = 0
-    @State private var restTimeInput: Int = 0
+@MainActor class TimerManager : ObservableObject {
+    @Published var workoutTimeRemaining = 0
+    @Published var superSetTimeRemaining = 0
+    @Published var restTimeRemaining = 0
     
-    var timer = Timer()
-    var globalTimer: Int {
-        workoutTimeInput
-    }
+    //@State var showWorkout = true
+    //@State var showSuper = true
+    //@State var showRest = true
     
-    //String to Int conversion returns optional
-    /*var workoutTime: Int {
-        return Int(workoutTimeInput) ?? 0
-    }
-    
-    var superSetTime: Int {
-        return Int(superSetTimeInput) ?? 0
-    }
-    
-    var restTime: Int {
-        return Int(restTimeInput) ?? 0
-    }*/
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Workout Timer Settings")) {
-                    Stepper("Main Set [\(workoutTimeInput) seconds]", value: $workoutTimeInput, in: 0...150)
-                    Stepper("Superset [\(superSetTimeInput) seconds]", value: $superSetTimeInput, in: 0...150)
-                    Stepper("Rest [\(restTimeInput) seconds]", value: $restTimeInput, in: 0...150)
-                }
-                Section() {
-                    HStack {
-                        Button("START", action: startTimer)
-                        Spacer()
-                        Button("PAUSE", action: pauseTimer)
-                        Spacer()
-                        Button("RESET", action: resetTimer)
-                    }
-                    Label(String(globalTimer), image: "")
-                        .labelStyle(TitleOnlyLabelStyle())
-                    .padding()
-                }
-            }
-            .navigationTitle("Test")//globalTimer)
+    private var cancellable_workout : AnyCancellable?
+    private var cancellable_super : AnyCancellable?
+    private var cancellable_rest : AnyCancellable?
+
+    func clockManager(workoutTimer: Int, superSetTimer: Int, restTimer: Int) {
+        let operation1 = BlockOperation { [self] in
+            //showSuper.toggle()
+            //showRest.toggle()
+            self.workoutTimerFunction(workoutTimer: workoutTimer)
+            Thread.sleep(forTimeInterval: TimeInterval(workoutTimeRemaining))
+            print("Operation 1 completed")
         }
+
+        let operation2 = BlockOperation { [self] in
+            //showWorkout.toggle()
+            //showRest.toggle()
+            self.supersetTimerFunction(superSetTimer: superSetTimer)
+            Thread.sleep(forTimeInterval: TimeInterval(superSetTimeRemaining))
+            print("Operation 2 completed")
+        }
+        
+        let operation3 = BlockOperation { [self] in
+            //showWorkout.toggle()
+            //showSuper.toggle()
+            self.restTimerFunction(restTimer: restTimer)
+            Thread.sleep(forTimeInterval: TimeInterval(restTimeRemaining))
+            print("Operation 3 completed")
+        }
+
+        operation2.addDependency(operation1)
+        operation3.addDependency(operation2)
+
+        let queue = OperationQueue()
+        queue.addOperation(operation1)
+        queue.addOperation(operation2)
+        queue.addOperation(operation3)
     }
     
-    func startTimer() {
-//        convertTimeLogic()
-//
-//        timer.invalidate()
-//        //Create timer
-        //timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ContentView.timerClass), userInfo: nil, repeats: true)
-    }
-    
-    func pauseTimer() {
-//        timer.invalidate()
-
-    }
-    func resetTimer() {
-//        timer.invalidate()
-//        globalTimer = 0
-    }
-    func timerClass() {
-//        globalTimer -= globalTimer
-//        if (globalTimer == 0) {
-//            timer.invalidate()
-//        }
-    }
-    func convertTimeLogic() {
-//        globalTimer = workoutTime
-    }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-#if canImport(UIKit)
-extension View {
-    func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
-#endif
-*/
-
-class TimerManager : ObservableObject {
-    @Published var timeRemaining = 0
-    private var cancellable : AnyCancellable?
-    
-    func startTimer(initial: Int) {
-        timeRemaining = initial
-        cancellable = Timer.publish(every: 1, on: .main, in: .common)
+    func workoutTimerFunction(workoutTimer: Int) {
+        workoutTimeRemaining = workoutTimer
+        cancellable_workout = Timer.publish(every: 1, on: .main, in: .common)
             .autoconnect()
             .sink { _ in
-                self.timeRemaining -= 1
-                if self.timeRemaining == 0 {
-                    self.cancellable?.cancel()
+                self.workoutTimeRemaining -= 1
+                if self.workoutTimeRemaining == 0 {
+                    self.cancellable_workout?.cancel()
+                }
+            }
+    }
+    
+    func supersetTimerFunction(superSetTimer: Int) {
+        superSetTimeRemaining = superSetTimer
+        cancellable_super = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                self.superSetTimeRemaining -= 1
+                if self.superSetTimeRemaining == 0 {
+                    self.cancellable_super?.cancel()
+                }
+            }
+    }
+    
+    func restTimerFunction(restTimer: Int) {
+        restTimeRemaining = restTimer
+        cancellable_rest = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                self.restTimeRemaining -= 1
+                if self.restTimeRemaining == 0 {
+                    self.cancellable_rest?.cancel()
                 }
             }
     }
@@ -157,38 +93,36 @@ class TimerManager : ObservableObject {
 
 struct ContentView: View {
     @StateObject private var timeManager = TimerManager()
-    @State private var workoutTimeInput = 60
-    
+    @State private var workoutTimeInput = 5
+    @State private var superSetTimeInput = 4
+    @State private var restTimeInput = 3
+
     var body: some View {
         NavigationView {
             Form {
+                Section(header: Text("Timers")){
+                    Text(("Main Set: \(timeManager.workoutTimeRemaining)"))
+                        //.opacity(timeManager.showWorkout ? 1 : 0)
+                    Text(("Super Set: \(timeManager.superSetTimeRemaining)"))
+                        //.opacity(timeManager.showSuper ? 1 : 0)
+                    Text(("Rest: \(timeManager.restTimeRemaining)"))
+                        //.opacity(timeManager.showRest ? 1 : 0)
+                }
                 Section(header: Text("Workout Timer Settings")) {
                     Stepper("Main Set [\(workoutTimeInput) seconds]", value: $workoutTimeInput, in: 0...150)
-                    //Stepper("Superset [\(superSetTimeInput) seconds]", value: $superSetTimeInput, in: 0...150)
-                    //Stepper("Rest [\(restTimeInput) seconds]", value: $restTimeInput, in: 0...150)
+                    Stepper("Superset [\(superSetTimeInput) seconds]", value: $superSetTimeInput, in: 0...150)
+                    Stepper("Rest [\(restTimeInput) seconds]", value: $restTimeInput, in: 0...150)
                 }
                 Section() {
                     HStack {
                         Button("START") {
-                            timeManager.startTimer(initial: workoutTimeInput)
+                            timeManager.clockManager(workoutTimer: workoutTimeInput, superSetTimer: superSetTimeInput, restTimer: restTimeInput)
                         }
-                        //Spacer()
-                        //Button("PAUSE", action: pauseTimer)
-                        //Spacer()
-                        //Button("RESET", action: resetTimer)
                     }
                 }
             }
-            .navigationTitle("Time remaining: \(timeManager.timeRemaining)")//globalTimer)
+            .navigationBarTitle("Clock Application", displayMode: .inline)
         }
-        
-        /*Stepper("Input \(stepperValue)", value: $stepperValue, in: 0...150)
-        Button("Start") {
-            timeManager.startTimer(initial: stepperValue)
-        }
-        Label("\(timeManager.timeRemaining)", image: "")
-            .labelStyle(TitleOnlyLabelStyle())
-         */
     }
 }
 
